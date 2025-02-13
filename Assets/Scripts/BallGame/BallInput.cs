83efba541c6a936f6ab5ae3fb7e6da903414afa6
ToniBorrull <146344011+ToniBorrull@users.noreply.github.com>
2025-02-13 17:38:11 +0100

    inputs

--- Assets/Scripts/BallGame/BallInput.cs
+++ Assets/Scripts/BallGame/BallInput.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 [RequireComponent(typeof(BallController))]
 public class BallInput : MonoBehaviour
 {
+    NewInputs input;
     BallController controller;
     private void Start()
     {
         controller = GetComponent<BallController>();
+        input = new NewInputs();
+        input.Player.Enable();
+        input.UI.Enable();
     }
     void Update()
     {
-
+        controller.Move(input.Player.Move.ReadValue<Vector2>());
+        if (input.Player.Jump.WasPressedThisFrame())
+        {
+            controller.Jump();
+        }
     }
 }
