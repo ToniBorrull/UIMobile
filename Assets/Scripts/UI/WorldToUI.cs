203f467c7cbe573d37d2b2ef12fb7a5e6fde6d24
ToniBorrull <146344011+ToniBorrull@users.noreply.github.com>
2025-02-20 18:00:36 +0100

    UI Ball

--- Assets/Scripts/UI/WorldToUI.cs
+++ Assets/Scripts/UI/WorldToUI.cs
@@ -7,6 +7,7 @@ public class WorldToUI : MonoBehaviour
 {
     Camera cam;
     RectTransform rectTransform;
+    RectTransform canvasRect;
     public Transform target;
     private void Start()
     {
@@ -16,8 +17,37 @@ public class WorldToUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = cam.WorldToViewportPoint(target.position);
+         Vector3 pos = cam.WorldToViewportPoint(target.position);
+  
+        
+        
+        if(pos.z < 0)
+        {
+            pos.x = -pos.x;
+            pos.y = -pos.y;
+            pos *= 100000;
+        }
+
+        if(pos.x > 1)
+        {
+            pos.x = 1;
+        }
+        if(pos.y > 1)
+        {
+            pos.y = 1;
+        }
+        if (pos.x < 0)
+        {
+            pos.x = 0;
+        }
+        if (pos.y < 0)
+        {
+            pos.y = 0;
+        }
+       
         rectTransform.anchorMin = pos;
         rectTransform.anchorMax = pos;
+
+        Debug.Log(pos);
     }
 }
