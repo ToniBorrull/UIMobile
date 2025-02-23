b445bb07de01e7f06268f9ca76918107ea071c45
Toni <toni.borrull@enti.cat>
2025-02-23 20:04:48 +0100

    LateralMenu Basic behaviour

--- Assets/Scripts/UI/LateralMenu.cs
+++ Assets/Scripts/UI/LateralMenu.cs
@@ -1,33 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Windows;
+using UnityEngine.UIElements;
+using TMPro;
 
 public class LateralMenu : MonoBehaviour
 {
-    NewInputs input;
     public RectTransform menuPanel;
     public float transitionSpeed = 5f;
+    public TMP_Text toggleButton;
     private Vector2 hiddenPosition;
     private Vector2 visiblePosition;
     private bool isVisible = false;
+  
 
-    void Start()
+    void Start() 
     {
-        input = new NewInputs();
-        // Guarda la posición visible y oculta del menú
         visiblePosition = menuPanel.anchoredPosition;
         hiddenPosition = new Vector2(menuPanel.anchoredPosition.x - menuPanel.rect.width, menuPanel.anchoredPosition.y);
 
-        // Inicia en la posición oculta
         menuPanel.anchoredPosition = hiddenPosition;
     }
 
     public void ToggleMenu()
     {
-        StopAllCoroutines(); // Detiene cualquier transición en curso
-        StartCoroutine(AnimateMenu(isVisible ? hiddenPosition : visiblePosition));
+        StopAllCoroutines();
+        if (!isVisible)
+        {
+            toggleButton.text = "Close";
+            StartCoroutine(AnimateMenu(visiblePosition));
+        }
+        else if (isVisible)
+        {
+            toggleButton.text = "Open";
+            StartCoroutine(AnimateMenu(hiddenPosition));
+        }
         isVisible = !isVisible;
+        
     }
 
     IEnumerator AnimateMenu(Vector2 targetPosition)
@@ -37,7 +46,7 @@ public class LateralMenu : MonoBehaviour
             menuPanel.anchoredPosition = Vector2.Lerp(menuPanel.anchoredPosition, targetPosition, Time.deltaTime * transitionSpeed);
             yield return null;
         }
-        menuPanel.anchoredPosition = targetPosition; // Ajusta la posición final
+        menuPanel.anchoredPosition = targetPosition; 
     }
 
 }
